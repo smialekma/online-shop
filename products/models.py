125cc99b11eb71c4cece276cbfd12da8a70197bc
smialekma <smialekma@gmail.com>
2025-02-15 10:12:52 +0100

    feat: added migrations.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from PIL import Image
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField()
+    description = models.TextField()
+    photo = models.ImageField()
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
