ed17f16c58c06805887e2c0e643ab90e008b1390
smialekma <smialekma@gmail.com>
2025-02-19 16:30:27 +0100

    feat(category): added category names and pics to home template.

--- products/models.py
+++ products/models.py
@@ -7,6 +7,16 @@ class Category(models.Model):
     name = models.CharField()
     photo = models.ImageField(upload_to="category_pics")
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.photo.path)
+
+        if img.height > 400 or img.width > 600:
+            output_size = (400, 600)
+            img.thumbnail(output_size)
+            img.save(self.photo.path)
+
     def __str__(self):
         return self.name
 
@@ -20,6 +30,16 @@ class Product(models.Model):
     quantity = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     date_added = models.DateTimeField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.photo.path)
+
+        if img.height > 600 or img.width > 600:
+            output_size = (600, 600)
+            img.thumbnail(output_size)
+            img.save(self.photo.path)
 
     def __str__(self):
         return self.name
