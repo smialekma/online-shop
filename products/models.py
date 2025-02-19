ca1b7dae6dbe045a01c840bd67144738952b9575
smialekma <smialekma@gmail.com>
2025-02-19 15:08:26 +0100

    feat(category): add category and link to products.

--- products/models.py
+++ products/models.py
@@ -3,13 +3,24 @@ from django.utils import timezone
 from PIL import Image
 
 
-class Product(models.Model):
+class Category(models.Model):
     name = models.CharField()
+    photo = models.ImageField(upload_to="category_pics")
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    brand = models.CharField()
     description = models.TextField()
-    photo = models.ImageField()
+    photo = models.ImageField(upload_to="product_pics")
     price = models.IntegerField()
     quantity = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     date_added = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
+
