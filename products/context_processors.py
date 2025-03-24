d91c8c5d2b4158b83f4a79abad523b3dbc963b3a
smialekma <smialekma@gmail.com>
2025-03-24 15:46:03 +0100

    feat(filter): added filters for categories, price and brands in product view.

--- products/context_processors.py
+++ products/context_processors.py
@@ -1,10 +1,5 @@
 from .models import Category
-from carts.cart import Cart
 
 
 def categories(request):
-    return {'categories': Category.objects.all().prefetch_related('products')}
-
-
-def cart(request):
-    return {"cart": Cart(request)}
+    return {"categories": Category.objects.all().prefetch_related("products")}
