d0aa74bec5aa8b03fe62ee82e92e31e411318ec7
smialekma <smialekma@gmail.com>
2025-10-11 10:23:31 +0200

    fix: added stripe secret keys to CI

--- management_panel/views.py
+++ management_panel/views.py
@@ -1,8 +1,9 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import Sum
 from django.views.generic import TemplateView
+from django.utils import timezone
 
 from orders.models import ShippingMethod, Order
 from payments.models import Payment
@@ -19,32 +20,35 @@ class ManagementPanelView(LoginRequiredMixin, UserPassesTestMixin, TemplateView)
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
 
-        context["total_products"] = Product.objects.all().count()
-        context["brands_count"] = Brand.objects.all().count()
-        context["categories_count"] = Category.objects.all().count()
-        context["shipping_methods_count"] = ShippingMethod.objects.all().count()
+        context["total_products"] = Product.objects.count()
+        context["brands_count"] = Brand.objects.count()
+        context["categories_count"] = Category.objects.count()
+        context["shipping_methods_count"] = ShippingMethod.objects.count()
 
-        context["pending_payments"] = (
-            Payment.objects.all().filter(is_paid=False).count()
-        )
+        context["pending_payments"] = Payment.objects.filter(is_paid=False).count()
 
-        today = datetime.now()
+        today = timezone.now()
         today_minus_30d = today - timedelta(days=30)
 
-        context["orders_count_30d"] = (
-            Order.objects.all().filter(created_at__gt=today_minus_30d).count()
-        )
+        context["orders_count_30d"] = Order.objects.filter(
+            created_at__gt=today_minus_30d
+        ).count()
         context["revenue_30d"] = (
-            Payment.objects.all()
-            .filter(is_paid=True, created_at__gt=today_minus_30d)
-            .aggregate(Sum("amount"))["amount__sum"]
-        )
+            Payment.objects.filter(
+                is_paid=True, created_at__gt=today_minus_30d
+            ).aggregate(Sum("amount"))["amount__sum"]
+        ) or 0
 
-        context["recent_orders"] = (
-            Order.objects.all().select_related("customer").order_by("-created_at")[:5]
-        )
-        context["recent_reviews"] = (
-            Review.objects.all().select_related("author").order_by("-created_at")[:2]
-        )
+        context["recent_orders"] = Order.objects.select_related("customer").order_by(
+            "-created_at"
+        )[:5]
+        context["recent_reviews"] = Review.objects.select_related("author").order_by(
+            "-created_at"
+        )[:2]
 
         return context
+
+    # def _get_recent_items(self):
+    #     return {
+    #         "recent_reviews": Review.objects.select_related("author").order_by("-created_at")[:2]
+    #     }
