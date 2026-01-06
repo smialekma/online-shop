0564a2c5f29a8133eedfae44bf28ce2a2251012a
smialekma <smialekma@gmail.com>
2026-01-06 13:21:30 +0100

    feat: created celery task for periodic newsletter sending

---
+++ newsletter/tasks.py
@@ -0,0 +1,23 @@
+from django.core.mail import EmailMessage
+
+from newsletter.models import Subscriber, NewsletterPost
+
+from celery import shared_task
+
+
+@shared_task
+def send_newsletter():
+    active_subscribers = Subscriber.objects.filter(is_active=True)
+    post_to_send = (
+        NewsletterPost.objects.filter(is_published=False, to_publish=True)
+        .order_by("-created_at")
+        .first()
+    )
+
+    mail_subject = post_to_send.title
+    message = post_to_send.body
+
+    for subscriber in active_subscribers:
+        to_email = subscriber.email
+        email = EmailMessage(mail_subject, message, to=[to_email])
+        email.send()
