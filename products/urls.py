64ab19b528749b059ef027db34f9783f04d2851e
smialekma <smialekma@gmail.com>
2026-01-31 21:58:49 +0100

    feat(product details): implemented adding multiple items to cart

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import ProductView, ProductDetailView, add_to_cart1, remove_from_cart
+from .views import (
+    ProductView,
+    ProductDetailView,
+    add_to_cart,
+    remove_from_cart,
+    add_to_cart_with_qty,
+)
 
 urlpatterns = [
     path("products/", ProductView.as_view(), name="product-view"),
     path("products/<int:pk>", ProductDetailView.as_view(), name="detail-view"),
-    path("products/add", add_to_cart1, name="product-add"),
+    path("products/<int:pk>/add", add_to_cart_with_qty, name="detail-add"),
+    path("products/add", add_to_cart, name="product-add"),
     path("products/remove", remove_from_cart, name="product-remove"),
 ]
